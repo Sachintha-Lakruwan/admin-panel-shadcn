09c37361e62bdb396eeeffeb3b3dc6d205a23e33
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-10-30 23:17:55 +0530

    Refactor RevenueCard component to include padding and text alignment

--- app/total_revenue/page.tsx
+++ app/total_revenue/page.tsx
@@ -4,60 +4,61 @@ import React, { useEffect, useState } from "react";
 import RevenueCard from "./RevenueCard";
 import { RevenueCardProps } from "./RevenueCard";
 
-const data: RevenueCardProps[] = [
-  {
-    name: "Boeing 747",
-    revenue: 100000,
-  },
-  {
-    name: "Boeing 737",
-    revenue: 200000,
-  },
-  {
-    name: "Airbus A380",
-    revenue: 300000,
-  },
-  {
-    name: "Airbus A320",
-    revenue: 400000,
-  },
-  {
-    name: "Boeing 747",
-    revenue: 100000,
-  },
-  {
-    name: "Boeing 737",
-    revenue: 200000,
-  },
-  {
-    name: "Airbus A380",
-    revenue: 300000,
-  },
-  {
-    name: "Airbus A320",
-    revenue: 400000,
-  },
-  {
-    name: "Boeing 747",
-    revenue: 100000,
-  },
-  {
-    name: "Boeing 737",
-    revenue: 200000,
-  },
-  {
-    name: "Airbus A380",
-    revenue: 300000,
-  },
-  {
-    name: "Airbus A320",
-    revenue: 400000,
-  },
-];
+// const data: RevenueCardProps[] = [
+//   {
+//     name: "Boeing 747",
+//     revenue: 100000,
+//   },
+//   {
+//     name: "Boeing 737",
+//     revenue: 200000,
+//   },
+//   {
+//     name: "Airbus A380",
+//     revenue: 300000,
+//   },
+//   {
+//     name: "Airbus A320",
+//     revenue: 400000,
+//   },
+//   {
+//     name: "Boeing 747",
+//     revenue: 100000,
+//   },
+//   {
+//     name: "Boeing 737",
+//     revenue: 200000,
+//   },
+//   {
+//     name: "Airbus A380",
+//     revenue: 300000,
+//   },
+//   {
+//     name: "Airbus A320",
+//     revenue: 400000,
+//   },
+//   {
+//     name: "Boeing 747",
+//     revenue: 100000,
+//   },
+//   {
+//     name: "Boeing 737",
+//     revenue: 200000,
+//   },
+//   {
+//     name: "Airbus A380",
+//     revenue: 300000,
+//   },
+//   {
+//     name: "Airbus A320",
+//     revenue: 400000,
+//   },
+// ];
 
 export default function Page() {
-  const [revenueDetals, setRevenueDetails] = useState<RevenueCardProps[]>(data);
-
+  const [revenueDetals, setRevenueDetails] = useState<
+    RevenueCardProps[] | null
+  >(null);
 
   async function fetchRevenueDetails() {
     const res = await fetch(`/api/revenue`);
@@ -71,13 +72,14 @@ export default function Page() {
 
   return (
     <div className=" p-8 h-full overflow-scroll grid grid-cols-4 gap-6 overflow-x-hidden">
-      {revenueDetals.map((i) => (
-        <RevenueCard
-          key={crypto.randomUUID()}
-          name={i.name}
-          revenue={i.revenue}
-        />
-      ))}
+      {revenueDetals &&
+        revenueDetals.map((i) => (
+          <RevenueCard
+            key={crypto.randomUUID()}
+            name={i.name}
+            revenue={i.revenue}
+          />
+        ))}
     </div>
   );
 }
