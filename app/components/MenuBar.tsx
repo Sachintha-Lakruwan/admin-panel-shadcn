8bc319a7d62ef2d9113e1d5189e06f9682ecab16
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-10-30 17:01:27 +0530

    Refactor MenuBar component grid layout

--- app/components/MenuBar.tsx
+++ app/components/MenuBar.tsx
@@ -5,7 +5,7 @@ export default function MenuBar({ children }: { children: React.ReactNode }) {
   return (
     <div className=" w-full h-screen">
       <div className=" w-full h-[10%] bg-zinc-100"></div>
-      <div className=" h-[90%] w-full grid grid-cols-[1fr_4fr]">
+      <div className=" h-[90%] w-full grid grid-cols-[1fr_3fr]">
         <div className=" w-full h-full">
           <UserSideBar />
         </div>
